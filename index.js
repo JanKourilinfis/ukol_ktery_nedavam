7eaeb36c2100e035246e30cd7b8ea63a6b6f52dd
unknown <jan.kouril@infis.cz>
2025-06-09 13:58:43 +0200

    hotovo

--- index.js
+++ index.js
@@ -3,7 +3,32 @@ async function italianB(){
     const data = await response.json()
     const div =document.getElementById("div")
     const fotka=document.createElement("img")
-    fotka.src=data.imageURL
+    fotka.src=data.imageUrl
+    console.log(data.imageUrl)
     div.append(fotka)
+}
+async function posliB(){
+    const dataToSend = {
+        imageUrl: document.getElementById("odkaz").value,
+        name: document.getElementById("jmeno").value,
+        description: document.getElementById("popis").value,
+        author: document.getElementById("autor").value
+    }
+    console.log("Odesílám: " + JSON.stringify(dataToSend))
+    const response = await fetch("https://api.janmotlik.dev/add-brainrot", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: 
+        JSON.stringify(dataToSend)
+    })
+    if (!response.ok){
+        const errorText = await response.text();
+        alert("Chyba kódu " +response.status +" - " + errorText);
+        return;
+    }
+    const data = response.text;
+    console.log("server odpověděl: " +data);
 }
 
